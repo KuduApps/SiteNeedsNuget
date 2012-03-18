255161e3833986ebafed79255db20c572c42be24
Xun Sun <xusun@microsoft.com>
2012-03-17 17:03:42 -0700

    initial commit

---
+++ SiteNeedNuget/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace SiteNeedNuget.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Newtonsoft.Json.JsonSerializer ser = new Newtonsoft.Json.JsonSerializer();
+
+            string json = JsonConvert.SerializeObject(new Foo { Bar = "a"});
+
+            ViewBag.Message = "Welcome to ASP.NET MVC!" + json;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+
+    }
+    public class Foo
+    {
+        public string Bar { get; set; }
+    }
+}
